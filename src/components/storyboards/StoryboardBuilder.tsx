5a76736c55432e46aa1aa0f3b8fa21b805880553
Rijurekh Ghosh <93432199+rijurekh24@users.noreply.github.com>
2025-08-21 17:14:42 +0000

    final

--- src/components/storyboards/StoryboardBuilder.tsx
+++ src/components/storyboards/StoryboardBuilder.tsx
@@ -16,11 +16,15 @@ export interface Storyboard extends StoryboardInfo {
   }[];
 }
 
-export default function StoryboardBuilder({ storyboards }: { storyboards: Storyboard[] }) {
+export default function StoryboardBuilder({
+  storyboards,
+}: {
+  storyboards: Storyboard[];
+}) {
   const [selected, setSelected] = useState<Storyboard | null>(null);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-2">
       {storyboards.map((storyboard) => (
         <div key={storyboard.id} onClick={() => setSelected(storyboard)}>
           <StoryboardCard {...storyboard} />
@@ -28,7 +32,11 @@ export default function StoryboardBuilder({ storyboards }: { storyboards: Storyb
       ))}
 
       {selected && (
-        <StoryboardModal storyboard={selected} isOpen={true} onClose={() => setSelected(null)} />
+        <StoryboardModal
+          storyboard={selected}
+          isOpen={true}
+          onClose={() => setSelected(null)}
+        />
       )}
     </div>
   );
